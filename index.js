7b110c6f389cd387b00397a49321162f10c15d3e
Krychaxp <krychaxp@op.pl>
2020-11-10 13:23:41 +0100

    fixed cloning pictures and sorting merge

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 (async () => {
-  console.time()
+  console.time();
+  const ImagesToPDF = require("images-pdf");
   const { exec } = require("child_process");
-  const imagesToPdf = require("images-to-pdf");
   const fs = require("fs");
   const path = require("path");
   const png_folder = "png";
   const captureWebsite = require("capture-website");
+  const axios=require('axios')
   let i = 1;
   try {
     const url = fs
@@ -13,30 +14,28 @@
       .match(/^(.*\/)(\d*)$/)[1];
     console.log("Start...");
     if (fs.existsSync(png_folder)) {
-      //remove all images from folder
       fs.readdirSync(png_folder).forEach((v) =>
         fs.unlinkSync(path.join(png_folder, v))
       );
     } else {
-      // add folder 'png'
       fs.mkdirSync(png_folder);
     }
-    while (true) {
-      await captureWebsite.file(url+i, path.join(png_folder, `${i}.png`), {
+    while(true){
+      await captureWebsite.file(url + i, path.join(png_folder, `${(i+[]).padStart(3,0)}.png`), {
         scaleFactor: 3,
-        timeout:20,
-        element: "#surface1",
+        timeout: 30,
+        element: "svg",
       });
       console.log("> dodano slajd: " + i);
-      i++;
+      i++
     }
   } catch (e) {
     console.log("Koniec!");
   } finally {
     if (i > 1) {
       const merged_url = `p-bbb-${new Date().toLocaleDateString()}.pdf`;
-      imagesToPdf(
-        fs.readdirSync("png").map((v) => path.join(png_folder, v)),
+      new ImagesToPDF.ImagesToPDF().convertFolderToPDF(
+        png_folder,
         merged_url
       );
       console.log(`Pomyślnie utworzono prezentacje o nazwie: '${merged_url}'`);
@@ -44,7 +43,7 @@
       console.log("Błędny link");
     }
     console.log("Możesz zamknąć już to okno ;D");
-    console.timeEnd()
+    console.timeEnd();
     exec("pause");
   }
 })();
