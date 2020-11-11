b22bceff9188f70509052850d20232458707fa3d
Krychaxp <krychaxp@op.pl>
2020-11-11 14:28:36 +0100

    faster

--- index.js
+++ index.js
@@ -5,10 +5,9 @@
   const fs = require("fs");
   const path = require("path");
   const png_folder = "png";
-  const captureWebsite = require("capture-website");
   const puppeteer = require("puppeteer");
-  const axios = require("axios");
   let i = 1;
+  let browser;
   try {
     const url = fs
       .readFileSync("file_url.txt", "utf-8")
@@ -21,32 +20,33 @@
     } else {
       fs.mkdirSync(png_folder);
     }
-    const browser = await puppeteer.launch();
+    browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.setViewport({
+      width: 1700,
+      height: 1300,
+      deviceScaleFactor: 1,
+    });
     while (true) {
-      await page
-        .mainFrame()
-        .addStyleTag({ content: "svg{width:100% !important}" });
-      //   await page.setViewport({
-      //     width: 1700,
-      //     height: 1300,
-      //     deviceScaleFactor: 1,
-      //   });
+      // await page
+      //   .mainFrame()
+      //   .addStyleTag({ content: "svg{width:100% !important}" });
 
-      await page.goto(
-        `https://bbb-one.put.poznan.pl/bigbluebutton/presentation/01e1a95db3af9409253f5ba623afae95b7ef0276-1604997346165/01e1a95db3af9409253f5ba623afae95b7ef0276-1604997346165/7217cfeab04431aac529c1bf1d45136b42bfa306-1604997550782/svg/${i}`
-      );
-      if (res.request() == 404) {
-        throw Error("koniec");
+      let res = await page.goto(url + i);
+      console.log(res,page)
+      if (res.status() != 200) {
+        throw new Error("koniec");
       }
-      await page.screenshot({ path: path.join(png_folder,`${i+[].pad}.png`)});
+      await page.screenshot({
+        path: path.join(png_folder, `${(i + []).padStart(3, 0)}.png`),
+      });
       console.log("> dodano slajd: " + i);
       i++;
     }
-    await browser.close();
   } catch (e) {
     console.log("Koniec!");
   } finally {
+    await browser.close();
     if (i > 1) {
       const merged_url = `p-bbb-${new Date().toLocaleDateString()}.pdf`;
       new ImagesToPDF.ImagesToPDF().convertFolderToPDF(png_folder, merged_url);
